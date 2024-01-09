47764ef0926ab3cde38406d6cde97d9dd940f038
pych1971 <pych1971@gmail.com>
2024-01-09 22:00:42 +0500

    090120242200

--- urok.py
+++ urok.py
@@ -1,10 +1,46 @@
 import sqlite3
 
 """Подключение к существующей базе данных"""
-db = sqlite3.connect(r'c:\Users\Oleg\YandexDisk\Programming\SQL\qa_testing.db')  # Подключение к базе данных
+# db = sqlite3.connect(r'c:\Users\Oleg\YandexDisk\Programming\SQL\qa_testing.db')  # Подключение к базе данных
+# print('Подключились к базе данных')
+# cur = db.cursor()  # Переменная для управления базой данных
+# cur.execute("""SELECT * FROM Students;""")  # Запрос для получения содержимого таблицы Students
+# result = cur.fetchall()  # Результат запроса
+# print(result)
+# print(type(result))
+"""Создание новой базы данных"""
+db = sqlite3.connect('test_sql.db')  # Создание базы данных
 print('Подключились к базе данных')
 cur = db.cursor()  # Переменная для управления базой данных
-cur.execute("""SELECT * FROM Students;""")  # Запрос для получения содержимого таблицы Students
-result = cur.fetchall()  # Результат запроса
-print(result)
-print(type(result))
+"""Создание таблицы"""
+# cur.execute("""CREATE TABLE IF NOT EXISTS Students(
+#     StudentsID INTEGER PRIMARY KEY,
+#     First_name TEXT NOT NULL,
+#     Last_name TEXT NOT NULL);
+# """)
+# db.commit()  # Сохренение запроса
+# print('Создание таблицы Students')
+# """Заполнение таблицы Students"""
+# cur.execute("""INSERT INTO Students(First_name, Last_name)
+#     VALUES('Petr', 'Petrov');""")
+# db.commit()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS Students1(
+    StudentsID INTEGER PRIMARY KEY AUTOINCREMENT,
+    First_name TEXT NOT NULL,
+    Last_name TEXT NOT NULL);
+""")
+db.commit()  # Сохренение запроса
+print('Создание таблицы Students')
+# """НЕБЕЗОПАСНЫЙ СПОСОБ - Заполнение таблицы Students"""
+# cur.execute("""INSERT INTO Students1(First_name, Last_name)
+#     VALUES('Petr', 'Petrov');""")
+# db.commit()
+
+"""БЕЗОПАСНЫЙ СПОСОБ - Заполнение таблицы Students"""
+# date_students = ('Semen', 'Semenov')
+date_students = [('Alex', 'Aleksandrov'), ('Olga', 'Olgina')]
+cur.executemany("""INSERT INTO Students1(First_name, Last_name)
+    VALUES(?, ?);""", date_students)
+db.commit()
+print('Добавление новых данных в таблицу')
